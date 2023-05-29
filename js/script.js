db10a8d8dd32d45c252f5dfc8821772822240f16
Tommicavo <tommicava@icloud.com>
2023-05-29 14:10:49 +0200

    form

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 console.log("js ok");
 
+const passenger = document.getElementById("passenger");
+const kilometers = document.getElementById("kilometers");
+const age = document.getElementById("age");
+const button = document.getElementById("generate");
+
+button.addEventListener("click", function(){
+    const myPassenger = passenger.value;
+    const myKilometers = kilometers.value;
+    const myAge = age.value;
+
+    console.log(myPassenger);
+    console.log(myKilometers);
+    console.log(myAge);
+});
